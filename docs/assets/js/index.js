890164deedff76c4ec9d7419b47bd1251c8a2aa6
knightyun <2386209384@qq.com>
2023-09-17 20:14:00 +0800

    update

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,43 +1,60 @@
 const convertInput = document.querySelector('.convert-input');
 const convertBtn = document.querySelector('.convert');
 const encryptBtn = document.querySelector('.encrypt');
+const decryptBtn = document.querySelector('.decrypt');
 const copyBtn = document.querySelector('.copy');
-const exp = document.querySelector('.expression');
-const result = document.querySelector('.result-msg');
+const expBox = document.querySelector('.expression');
+const resultBox = document.querySelector('.result-msg');
 let clickable = true;
 
-function startConvert(isJs = false) {
+function startConvert(type) {
     const inputText = convertInput.value.trim();
 
     if (inputText) {
-        exp.innerText = magicExpression(inputText, isJs);
-        result.innerText = isJs ? inputText : '"' + inputText + '"';
+        const { result } = new MagicExpression(inputText, type);
+
+        expBox.innerText = result;
+        resultBox.innerText =
+            type === 'data'
+                ? `"${inputText}"`
+                : type === 'decrypt'
+                ? result
+                : inputText;
     }
     convertInput.value = '';
 }
 
-// 开始转换
-convertBtn.onclick = function() { startConvert(); };
-encryptBtn.onclick = function() { startConvert(true); };
+// 数据转换
+convertBtn.onclick = function () {
+    startConvert('data');
+};
+// js 加密
+encryptBtn.onclick = function () {
+    startConvert('encrypt');
+};
+// js 解密
+decryptBtn.onclick = function () {
+    startConvert('decrypt');
+};
 
 // 选中并复制代码
-copyBtn.onclick = function() {
+copyBtn.onclick = function () {
     if (clickable) {
         clickable = false;
         const that = this;
         const tmp = that.innerText;
         const range = document.createRange();
         const sel = getSelection();
-    
-        range.selectNode(exp);
+
+        range.selectNode(expBox);
         sel.removeAllRanges();
         sel.addRange(range);
         document.execCommand('copy');
         that.innerText = '复制成功！';
-        setTimeout(function() {
+        setTimeout(function () {
             that.innerText = tmp;
             sel.removeRange(range);
             clickable = true;
         }, 2000);
     }
-}
+};
--- docs/assets/js/magic-expression.js
+++ docs/assets/js/magic-expression.js
@@ -1,103 +1,166 @@
 /**
- * 主函数
- * @param {string} str - 要转换的字符串
- * @param {boolean} isJs - 是否为js加密
- * @returns {string}
+ * @file JS 混淆、加/解密算法. (https://github.com/knightyun/magic-expression)
+ * @copyright 2019 knightyun. <https://github.com/knightyun/magic-expression/raw/master/docs/assets/js/magic-expression.js>
+ * @license MIT License. <https://raw.githubusercontent.com/knightyun/magic-expression/master/LICENSE>
  */
-function magicExpression(str, isJs = false) {
-    // const charUniCodes = []; // 保存每个字符的 Unicode 符
-
-    // 构造 "[]['constructor']['constructor']('return '+'"'+'\\uxxxx'+'"')()" 字符串
-    const _constructor =
-        toExp('c') + '+' +
-        toExp('o') + '+' +
-        toExp('n') + '+' +
-        toExp('s') + '+' +
-        toExp('t') + '+' +
-        toExp('r') + '+' +
-        toExp('u') + '+' +
-        toExp('c') + '+' +
-        toExp('t') + '+' +
-        toExp('o') + '+' +
-        toExp('r');
-
-    const _return =
-        toExp('r') + '+' +
-        toExp('e') + '+' +
-        toExp('t') + '+' +
-        toExp('u') + '+' +
-        toExp('r') + '+' +
-        toExp('n') + '+' +
-        toExp(' ');
-
-    const resultBody = str.replace(isJs ? /[\r\n]/g : '', '').split('').map(s => toExp(s));
-
-    const result = '[][' + _constructor + '][' + _constructor + '](' + _return + '+\'"\'+' + resultBody.join('+') + '+\'"\')()';
-    const encryptedJs = '[][' + _constructor + '][' + _constructor + '](' + result + ')()';
-
-    return isJs ? encryptedJs : result;
-}
 
 /**
- * 十六进制数字字符转换为 Unicode 符格式
- * @param {string} hexCode 
- * @returns {string}
+ * JS 混淆、加/解密算法
+ * @class MagicExpression
  */
-function toUnicode(hexCode) {
-    switch (hexCode.length) {
-        case 4:
-            break;
-        case 3:
-            hexCode = '0' + hexCode;
-            break;
-        case 2:
-            hexCode = '00' + hexCode;
-            break;
-        case 1:
-            hexCode = '000' + hexCode;
-            break;
+class MagicExpression {
+    /** @type {string} - 转换结果 */
+    result = '';
+
+    _constructor = '';
+    _return = '';
+
+    /**
+     * @constructor
+     * @param {string} str - 要转换的字符串
+     * @param {boolean} type - 转换的类型, data | encrypt | decrypt
+     * @memberof MagicExpression
+     */
+    constructor(str, type) {
+        if (!str || !type) return;
+        // const charUniCodes = []; // 保存每个字符的 Unicode 符
+
+        // 构造 "[]['constructor']['constructor']('return '+'"'+'\\uxxxx'+'"')()" 这样的字符串
+        this._constructor = 'constructor'
+            .split('')
+            .map((s) => this.toExp(s))
+            .join('+');
+
+        this._return = 'return'
+            .split('')
+            .map((s) => this.toExp(s))
+            .join('+');
+
+        switch (type) {
+            case 'data':
+                this.result = this.dataTransfer(str);
+                break;
+            case 'encrypt':
+                this.result = this.encryptJs(str);
+                break;
+            case 'decrypt':
+                this.result = this.decryptJs(str);
+                break;
+            default:
+                break;
+        }
     }
-    return '\\u' + hexCode;
-}
 
-/**
- * 单个字符转换
- * @param {string} char 
- * @returns {string}
- */
-function toExp(char) {
-    // 已知的表达式映射 - 待完善
-    const charExps = {
-        ' ': '([]+{})[!![]+!![]+!![]+!![]+!![]+!![]+!![]]',
-        '\\': '([]+/\\\\/)[+!![]]',
-        '0': '[]+(+[])',
-        '1': '[]+(+!![])',
-        '2': '[]+(!![]+!![])',
-        '3': '[]+(!![]+!![]+!![])',
-        '4': '[]+(!![]+!![]+!![]+!![])',
-        '5': '[]+(!![]+!![]+!![]+!![]+!![])',
-        '6': '[]+(!![]+!![]+!![]+!![]+!![]+!![])',
-        '7': '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![])',
-        '8': '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![])',
-        '9': '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![])',
-        'a': '([]+![])[+!![]]',
-        'b': '([]+{})[!![]+!![]]',
-        'c': '([]+{})[!![]+!![]+!![]+!![]+!![]]',
-        'd': '([]+[][+[]])[!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]]',
-        'e': '([]+!![])[!![]+!![]+!![]]',
-        'f': '([]+![])[+[]]',
-        'n': '([]+[][+[]])[+!![]]',
-        'o': '([]+{})[+!![]]',
-        'r': '([]+!![])[+!![]]',
-        's': '([]+![])[!![]+!![]+!![]]',
-        't': '([]+!![])[+[]]',
-        'u': '([]+!![])[!![]+!![]]'
-    };
+    /**
+     * 数据转换
+     * @param {string} str - 要转换的字符串
+     * @returns {string}
+     */
+    dataTransfer(str) {
+        const { _constructor, _return } = this;
+        const resultBody = str
+            .replace(/\\/g, '\\\\') // 处理转义符 '\' 的问题
+            .split('')
+            .map((s) => this.toExp(s))
+            .join('+');
 
-    if (charExps[char]) return charExps[char];
+        return `[][${_constructor}][${_constructor}](${_return}+'\"'+${resultBody}+'\"')()`;
+    }
 
-    const hexNum = char.charCodeAt(0).toString(16);
-    const hex = toUnicode(hexNum);
+    /**
+     * JS 加密
+     * @param {string} str - 要加密的 js
+     * @returns {string}
+     */
+    encryptJs(str) {
+        const { _constructor } = this;
+        const data = this.dataTransfer(str);
+        return `[][${_constructor}][${_constructor}](${data})()`;
+    }
 
-    return hex.split('').map(x => toExp(x)).join('+');
+    /**
+     * JS 解密
+     * @param {string} str - 要解密的代码
+     * @returns {string}
+     */
+    decryptJs(str) {
+        const { _constructor } = this;
+        const prefix = `[][${_constructor}][${_constructor}](`
+            .split('')
+            .map((s) => '\\' + s)
+            .join('');
+        const suffix = `\\)\\(\\)`;
+        const reg = new RegExp(`(^${prefix})|(${suffix}$)`, 'g');
+
+        return eval(str.replace(reg, ''));
+    }
+
+    /**
+     * 十六进制数字字符转换为 Unicode 符格式
+     * @param {string} hexCode
+     * @returns {string}
+     * @memberof MagicExpression
+     */
+    toUnicode(hexCode) {
+        switch (hexCode.length) {
+            case 4:
+                break;
+            case 3:
+                hexCode = '0' + hexCode;
+                break;
+            case 2:
+                hexCode = '00' + hexCode;
+                break;
+            case 1:
+                hexCode = '000' + hexCode;
+                break;
+        }
+        return '\\u' + hexCode;
+    }
+
+    /**
+     * 单个字符转换
+     * @param {string} char
+     * @returns {string}
+     * @memberof MagicExpression
+     */
+    toExp(char) {
+        // 已知的表达式映射 - 待完善
+        const charExps = {
+            ' ': '([]+{})[!![]+!![]+!![]+!![]+!![]+!![]+!![]]',
+            '\\': '([]+/\\\\/)[+!![]]',
+            0: '[]+(+[])',
+            1: '[]+(+!![])',
+            2: '[]+(!![]+!![])',
+            3: '[]+(!![]+!![]+!![])',
+            4: '[]+(!![]+!![]+!![]+!![])',
+            5: '[]+(!![]+!![]+!![]+!![]+!![])',
+            6: '[]+(!![]+!![]+!![]+!![]+!![]+!![])',
+            7: '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![])',
+            8: '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![])',
+            9: '[]+(!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![])',
+            a: '([]+![])[+!![]]',
+            b: '([]+{})[!![]+!![]]',
+            c: '([]+{})[!![]+!![]+!![]+!![]+!![]]',
+            d: '([]+[][+[]])[!![]+!![]+!![]+!![]+!![]+!![]+!![]+!![]]',
+            e: '([]+!![])[!![]+!![]+!![]]',
+            f: '([]+![])[+[]]',
+            n: '([]+[][+[]])[+!![]]',
+            o: '([]+{})[+!![]]',
+            r: '([]+!![])[+!![]]',
+            s: '([]+![])[!![]+!![]+!![]]',
+            t: '([]+!![])[+[]]',
+            u: '([]+!![])[!![]+!![]]',
+        };
+
+        if (charExps[char]) return charExps[char];
+
+        const hexNum = char.charCodeAt(0).toString(16);
+        const hex = this.toUnicode(hexNum);
+
+        return hex
+            .split('')
+            .map((x) => this.toExp(x))
+            .join('+');
+    }
 }
